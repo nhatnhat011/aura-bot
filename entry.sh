3c5fe9410cacc34c627e46c3ccb51df0f78b63a4
nhatnhat011 <86827092+nhatnhat011@users.noreply.github.com>
2025-01-27 20:40:32 +0700

    Add files via upload

---
+++ entry.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+SCRIPT_PATH=${SCRIPT_PATH:-"/app/data/aura.cfg"}
+BOT_VIRTUAL_NAME=${BOT_VIRTUAL_NAME:-"|cFFFF0000Aura"}
+HOST_PORT=${HOST_PORT:-'6113'}
+SERVER=${SERVER:-"server.eurobattle.net"}
+SERVER_PORT=${SERVER_PORT:-'6112'}
+SERVER_ALIAS=${SERVER_ALIAS:-"EuroBattle"}
+BOT_USER_NAME=${BOT_USER_NAME:-"aura"}
+BOT_PASSWORD=${BOT_PASSWORD:-"aura"}
+ROOT_ADMINS=${ROOT_ADMINS:-"W3_Beta"}
+WAR_VERSION=${WAR_VERSION:-'28'}
+
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bot_virtualhostname'; then
+	echo "bot_virtualhostname = $BOT_VIRTUAL_NAME" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bot_hostport'; then
+	echo "bot_hostport = $HOST_PORT" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_server'; then
+	echo "bnet_server = $SERVER" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_serverport'; then
+	echo "bnet_serverport = $SERVER_PORT" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_serveralias'; then
+	echo "bnet_serveralias = $SERVER_ALIAS" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_username'; then
+	echo "bnet_username = $BOT_USER_NAME" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_password'; then
+	echo "bnet_password = $BOT_PASSWORD" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_rootadmins'; then
+	echo "bnet_rootadmins = $ROOT_ADMINS" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_custom_war3version'; then
+	echo "bnet_custom_war3version = $WAR_VERSION" >> "$SCRIPT_PATH"
+fi
+
+bash ./aura++ $SCRIPT_PATH
