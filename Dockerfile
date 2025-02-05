71cff0adc9f7a2f69286da696f5d81812ff9a88e
nhatnhat011 <86827092+nhatnhat011@users.noreply.github.com>
2025-02-05 20:59:58 +0700

    Update dockerfile build

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,18 @@
-FROM alpine:3.13.1 as build
-WORKDIR /src
-RUN apk add git build-base gmp-dev m4 bzip2-dev zlib-dev cmake &&\
-    git clone https://github.com/nhatnhat011/aura-bot.git &&\
+FROM alpine:3.13.1 AS build
+ADD https://github.com/ladislav-zezula/StormLib/archive/refs/tags/v9.30.tar.gz /src/stormlib.tar.gz
+COPY . /aura-bot
+RUN apk add build-base gmp-dev m4 bzip2-dev zlib-dev cmake && tar -xvf stormlib.tar.gz &&\
     mkdir aura-bot/StormLib/build && cd aura-bot/StormLib/build &&\
-    cmake -DCMAKE_BUILD_TYPE=Release -DBUILD_DYNAMIC_MODULE=1 .. && make && make install &&\
-    cd /src/aura-bot/bncsutil/src/bncsutil/ && make &&\
-    cd /src/aura-bot/ && make
+    cd StormLib-9.25 && cmake CMakeLists.txt && make && make install &&\
+    cd /aura-bot/bncsutil/src/bncsutil/ && make &&\
+    cd /aura-bot/ && make &&\
+    chmod +x entry.sh
 FROM alpine:latest
-COPY --from=build /src/aura-bot/StormLib/build/libstorm.a /src/aura-bot/bncsutil/src/bncsutil/libbncsutil.so /usr/lib
-COPY --from=build /src/aura-bot/aura++ /src/aura-bot/ip-to-country.csv /app/
-RUN apk add libbz2 gmp libstdc++
+COPY --from=build /aura-bot/StormLib-9.30/libstorm.a /aura-bot/bncsutil/src/bncsutil/libbncsutil.so /usr/lib
+COPY --from=build /aura-bot/aura++ /aura-bot/ip-to-country.csv /aura-bot/aura_example.cfg /aura-bot/entry.sh /app/
+COPY --from=build /aura-bot/wc3 /app/wc3
+COPY --from=build /aura-bot/mapcfgs /app/mapcfgs
+RUN apk add libbz2 gmp libstdc++ python3 py3-pip &&\
+    pip install gdown --break-system-packages
 WORKDIR /app
-CMD ["./aura++","./aura-bot/aura.cfg"]
+CMD ["./entry.sh"]
---
+++ entry.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+SCRIPT_PATH=${SCRIPT_PATH:-"/app/data/aura.cfg"}
+BOT_VIRTUAL_NAME=${BOT_VIRTUAL_NAME:-"|cFFFF0000Aura"}
+HOST_PORT=${HOST_PORT:-'6113'}
+RECONNECT_PORT=${RECONNECT_PORT:-'6114'}
+SERVER=${SERVER:-"server.eurobattle.net"}
+SERVER_PORT=${SERVER_PORT:-'6112'}
+SERVER_ALIAS=${SERVER_ALIAS:-"EuroBattle"}
+BOT_USER_NAME=${BOT_USER_NAME:-"aura"}
+BOT_PASSWORD=${BOT_PASSWORD:-"aura"}
+ROOT_ADMINS=${ROOT_ADMINS:-"W3_Beta"}
+WAR_VERSION=${WAR_VERSION:-'28'}
+
+if [ ! -e "$SCRIPT_PATH" ]; then
+	cp "/app/aura_example.cfg" "$SCRIPT_PATH"
+fi
+
+if [ ! -d "/app/data/wc3" ]; then
+	cp -r "/app/wc3" "/app/data/wc3"
+fi
+
+if [ ! -d "/app/data/mapcfgs" ]; then
+	cp -r "/app/mapcfgs" "/app/data/mapcfgs"
+fi
+
+if [ ! -d "/app/data/maps" ]; then
+	mkdir "/app/data/maps"
+fi
+
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bot_virtualhostname'; then
+	echo "bot_virtualhostname = $BOT_VIRTUAL_NAME" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bot_hostport'; then
+	echo "bot_hostport = $HOST_PORT" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bot_reconnectport'; then
+	echo "bot_reconnectport = $RECONNECT_PORT" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_server'; then
+	echo "bnet_server = $SERVER" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_serverport'; then
+	echo "bnet_serverport = $SERVER_PORT" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_serveralias'; then
+	echo "bnet_serveralias = $SERVER_ALIAS" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_username'; then
+	echo "bnet_username = $BOT_USER_NAME" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_password'; then
+	echo "bnet_password = $BOT_PASSWORD" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_rootadmins'; then
+	echo "bnet_rootadmins = $ROOT_ADMINS" >> "$SCRIPT_PATH"
+fi
+if ! cat "$SCRIPT_PATH" 2>/dev/null | grep -q 'bnet_custom_war3version'; then
+	echo "bnet_custom_war3version = $WAR_VERSION" >> "$SCRIPT_PATH"
+fi
+
+/app/aura++ $SCRIPT_PATH
